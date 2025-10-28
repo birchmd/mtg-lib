2be1b7c5b542773575c0986f2d68bac4bb0f8a60
Michael Birch <birchmd8@gmail.com>
2025-10-27 20:33:52 -0400

    Feat(sim): print average turn win

--- duskmourns-claim/src/main.rs
+++ duskmourns-claim/src/main.rs
@@ -4,6 +4,16 @@ mod simulation;
 
 fn main() {
     let (wins, losses) = parallelism::run(20, 100_000);
+    let mean_win = {
+        let (weighted_total, total) = wins
+            .iter()
+            .enumerate()
+            .fold((0, 0), |(wt, t), (n_turns, &freq)| {
+                (wt + (n_turns * freq), t + freq)
+            });
+        (weighted_total as f64) / (total as f64)
+    };
+    println!("Average {mean_win}");
     for (n_turns, amount) in wins.into_iter().enumerate() {
         println!("{n_turns},{amount}");
     }
